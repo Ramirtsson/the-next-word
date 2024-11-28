bfd13a246135d3f9123e880b0504f2bb5ec39f0e
Ramirtsson <rammirtson@gmail.com>
2024-11-27 23:07:47 -0600

    implementacion/json de palabras, clase de array y ajustes en escena game

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,15 +1,37 @@
 import Phaser from "../lib/phaser.js";
+import ArrayWord from "../../assets/js/array-word.js";
 
 export class GameScene extends Phaser.Scene{
     constructor(){
         super({key:'Game'});
+        this.arrayWord = []
+        this.formattedText = ""
+        this.nextWord = ""
     }
 
-    create(){
-        this.add
-        .text(this.scale.width / 2, this.scale.height / 2, 'Hello World', {
+    async create(){
+        this.nextWord = new ArrayWord("Sports");
+        await this.nextWord.loadData();
+
+        this.arrayWord = this.nextWord.getArrayWord()
+        console.log(this.arrayWord)
+
+        this.formattedText = this.arrayWord.map(row => row.join(' ')).join('\n');
+
+        this.textObject = this.add
+        .text(this.scale.width / 2, this.scale.height / 2, this.formattedText, {
           fontSize: '32px',
+          color: '#ffffff',
+          align: 'center',
         })
         .setOrigin(0.5);
+
+        setTimeout(()=>{
+            this.arrayWord = this.nextWord.getArrayWord()
+            console.log(this.arrayWord)
+
+            this.formattedText = this.arrayWord.map(row => row.join(' ')).join('\n');
+            this.textObject.setText(this.formattedText);
+        },5000)
     }
 }
